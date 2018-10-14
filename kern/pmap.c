bbe7d4fc4992cc8f2ab8931248390642e81cde4c
Yaoxiong Liang <yx.liangim@gmail.com>
2018-10-14 18:45:52 +0000

    temporary lab2

--- kern/pmap.c
+++ kern/pmap.c
@@ -102,8 +102,15 @@ boot_alloc(uint32_t n)
 	// to a multiple of PGSIZE.
 	//
 	// LAB 2: Your code here.
-
-	return NULL;
+    if (n == 0) return nextfree;
+    else if (n > 0) {
+        result = nextfree;
+        nextfree = ROUNDUP(nextfree + n, PGSIZE);
+        if((uint32_t)nextfree - KERNBASE > (npages*PGSIZE)) 
+            panic("Out Of Memory!\n");
+	    return result;
+    }
+    return NULL;
 }
 
 // Set up a two-level page table:
@@ -125,7 +132,7 @@ mem_init(void)
 	i386_detect_memory();
 
 	// Remove this line when you're ready to test this function.
-	panic("mem_init: This function is not finished\n");
+	//panic("mem_init: This function is not finished\n");
 
 	//////////////////////////////////////////////////////////////////////
 	// create initial page directory.
@@ -148,7 +155,8 @@ mem_init(void)
 	// array.  'npages' is the number of physical pages in memory.  Use memset
 	// to initialize all fields of each struct PageInfo to 0.
 	// Your code goes here:
-
+    pages = (struct PageInfo *) boot_alloc(npages * sizeof(struct PageInfo));
+    memset(pages, 0, npages * sizeof(struct PageInfo));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -252,8 +260,19 @@ page_init(void)
 	// NB: DO NOT actually touch the physical memory corresponding to
 	// free pages!
 	size_t i;
-	for (i = 0; i < npages; i++) {
-		pages[i].pp_ref = 0;
+    int num_alloc = ((uint32_t)boot_alloc(0) - KERNBASE) / PGSIZE;
+    int num_iohole = 96;
+
+    pages[0].pp_ref = 1;
+    for (i = 1; i < npages_basemem; i++) {
+        pages[i].pp_ref = 1;
+        pages[i].pp_link = page_free_list;
+        page_free_list = &pages[i];
+    }
+    for(i = npages_basemem; i < npages_basemem + num_iohole + num_alloc; i++)
+          pages[i].pp_ref = 1;
+    for (; i < npages; i++) {
+        pages[i].pp_ref = 1;
 		pages[i].pp_link = page_free_list;
 		page_free_list = &pages[i];
 	}
